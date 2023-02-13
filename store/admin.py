f4ebc7b425d1319cc9afe7def6a982d6397ae5ce
Gchin99 <gchin99@gmail.com>
2023-02-12 20:36:47 -0800

    register admin models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product #.models is the directory
 
 # Register your models here.
+# We are telling Django which models we want to register to use
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name,')}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price', 'in_stock', 'created', 'updated']
+    list_filter = ['in_stocK', 'is_active']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title',)}
