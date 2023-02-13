36a4dbbe61b40a116ba1534659be1040564ae29f
Gchin99 <gchin99@gmail.com>
2023-02-12 21:01:12 -0800

    created first line in database using the admin site

--- store/admin.py
+++ store/admin.py
@@ -7,11 +7,11 @@ from .models import Category, Product #.models is the directory
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name,')}
+    prepopulated_fields = {'slug': ('name',)}  #when we run the Django admin.  When creating a category, the name we give it will preporpulate  the slug 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'slug', 'price', 'in_stock', 'created', 'updated']
-    list_filter = ['in_stocK', 'is_active']
+    list_filter = ['in_stock', 'is_active']
     list_editable = ['price', 'in_stock']
     prepopulated_fields = {'slug': ('title',)}
